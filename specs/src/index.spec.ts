7049c80422f7c3bb44e909ce2323add6b55cfe69
anu3ev <alexander.s.anufriev@gmail.com>
2025-08-14 21:53:00 +0400

    Накидываем будущую структуру теста ImageEditor. Убираем ненужные jsdocs в классе.

---
+++ specs/src/index.spec.ts
@@ -0,0 +1,18 @@
+// Пример структуры тестов:
+describe('ImageEditor', () => {
+  describe('constructor', () => {
+    // тесты конструктора
+  })
+
+  describe('init', () => {
+    // тесты инициализации
+  })
+
+  describe('destroy', () => {
+    // тесты уничтожения
+  })
+
+  describe('_createMosaicPattern', () => {
+    // тесты создания паттерна
+  })
+})
