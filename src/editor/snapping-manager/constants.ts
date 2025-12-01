4afafc5eb226a0dda8cf9eaa0d893bfc7342ff6a
Alexander <alexander.anufriev@insales.ru>
2025-12-01 20:19:42 +0400

    guide color

--- src/editor/snapping-manager/constants.ts
+++ src/editor/snapping-manager/constants.ts
@@ -1,4 +1,4 @@
 export const SNAP_THRESHOLD = 5
-export const GUIDE_COLOR = '#ff00ff'
+export const GUIDE_COLOR = '#3D8BF4'
 export const GUIDE_WIDTH = 1
 export const IGNORED_IDS = ['montage-area', 'background', 'interaction-blocker']
