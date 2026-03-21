46e005308727641807558b7e33da734d4b8a3ab0
Alexander Anufriev <maggistr95@gmail.com>
2026-03-21 17:46:01 +0400

    version bump

--- src/demo/js/index.js
+++ src/demo/js/index.js
@@ -4,8 +4,8 @@ import initEditor from '../../main.js'
 document.addEventListener('DOMContentLoaded', async() => {
   // Инициализация редактора
   const editorInstance = await initEditor('editor', {
-    montageAreaWidth: 810,
-    montageAreaHeight: 1080,
+    montageAreaWidth: 512,
+    montageAreaHeight: 512,
     editorContainerWidth: '100%',
     editorContainerHeight: 'calc(100vh - 4rem)'
   })
